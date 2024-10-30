8a6cc1ce1aa0b8cb9214c8e4a456637ab10aff86
Iacob-Deniss-30127 <denissiacob2002@yahoo.com>
2024-10-30 18:33:05 +0200

    created edit courier

--- src/main/java/com/example/PackageDelivery/Controllers/CourierController.java
+++ src/main/java/com/example/PackageDelivery/Controllers/CourierController.java
@@ -3,11 +3,14 @@ package com.example.PackageDelivery.Controllers;
 import com.example.PackageDelivery.Entities.CourierEntity;
 import com.example.PackageDelivery.Services.CourierService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/couriers")
 @CrossOrigin("http://localhost:5173/")
 public class CourierController {
 
@@ -18,14 +21,30 @@ public class CourierController {
         this.courierService = courierService;
     }
 
-    @PostMapping("/api/couriers/create")
+    @PostMapping("/create")
     public CourierEntity createCourier(@RequestBody CourierEntity courierEntity) {
         return courierService.createCourier(courierEntity);
     }
 
-    @GetMapping("/api/couriers")
+    @GetMapping()
     public List<CourierEntity> getALlCouriers() {
         return courierService.getAllCouriers();
     }
 
+    @GetMapping("/{id}")
+    public CourierEntity getCourierByID(@PathVariable Integer id) {
+        return courierService.getCourierById(id);
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<CourierEntity> updateCourierById(
+            @PathVariable Integer id,
+            @RequestBody CourierEntity updatedCourier) {
+        try {
+            CourierEntity updatedEntity = courierService.editCourierById(updatedCourier, id);
+            return ResponseEntity.ok(updatedEntity);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
 }
--- src/main/java/com/example/PackageDelivery/Services/CourierService.java
+++ src/main/java/com/example/PackageDelivery/Services/CourierService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 
 @Service
@@ -23,6 +24,22 @@ public class CourierService {
 
     public List<CourierEntity> getAllCouriers() {
         return courierRepository.findAll();
+    }
+
+    public CourierEntity getCourierById(Integer id) {
+        return courierRepository.findById(id).get();
+    }
+
+    public CourierEntity editCourierById(CourierEntity updatedCourier, Integer id) {
+        try {
+            CourierEntity foundCourier = getCourierById(id);
+            if(updatedCourier.getPackages() != null) {
+                foundCourier.setPackages(updatedCourier.getPackages());
+            }
+            return courierRepository.save(foundCourier);
+        } catch (Exception e) {
+            throw new NoSuchElementException("The courier does not exist ", e);
+        }
     }
 
 }
